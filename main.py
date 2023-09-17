7cf09c73a7675991421c83f9b5aab816c5bfbeb8
billonium4535 <billy.sethgrimmett@gmail.com>
2023-09-17 17:26:22 +0100

    Added status

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="your status"))
     print(f'Logged in as {bot.user.name}')
 
 
